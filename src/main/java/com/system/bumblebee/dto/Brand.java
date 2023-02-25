e616b34fed5ac1b65f86651a7330aabcace0905b
HansiT9 <123957367+HansiT9@users.noreply.github.com>
2023-02-25 06:27:04 +0530

    updated brand and category classes

--- src/main/java/com/system/bumblebee/dto/Brand.java
+++ src/main/java/com/system/bumblebee/dto/Brand.java
@@ -1,4 +1,13 @@
 package com.system.bumblebee.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Brand {
+    private String brandName;
+    private String brandLogo;
 }
--- src/main/java/com/system/bumblebee/dto/Category.java
+++ src/main/java/com/system/bumblebee/dto/Category.java
@@ -1,4 +1,13 @@
 package com.system.bumblebee.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
+    private String categoryName;
+    private String categoryLogo;
 }
