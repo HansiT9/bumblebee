f21d075ed002c6362b04ef6ba44a42a508e015e5
HansiT9 <123957367+HansiT9@users.noreply.github.com>
2023-03-03 11:03:53 +0530

    Added cors configuration

---
+++ src/main/java/com/system/bumblebee/configuration/CorsConfiguration.java
@@ -0,0 +1,21 @@
+package com.system.bumblebee.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfiguration {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        org.springframework.web.cors.CorsConfiguration config = new org.springframework.web.cors.CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*"); // set the wildcard origin
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
